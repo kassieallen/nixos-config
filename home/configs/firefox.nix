a437ba7153a408a659868d85ee2fc37eca869509
kassieallen <kasallen.21@gmail.com>
2024-12-05 18:04:50 -0500

    removed unneeded extensions

--- home/configs/firefox.nix
+++ home/configs/firefox.nix
@@ -48,16 +48,6 @@ in {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
             installation_mode = "force_installed";
           };
-          # Privacy Badger:
-          "jid1-MnnxcxisBPnSXQ@jetpack" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
-            installation_mode = "force_installed";
-          };
-          # 1Password:
-          "{d634138d-c276-4fc8-924b-40a0ea21d284}" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/1password-x-password-manager/latest.xpi";
-            installation_mode = "force_installed";
-          };
         };
   
         /* ---- PREFERENCES ---- */
